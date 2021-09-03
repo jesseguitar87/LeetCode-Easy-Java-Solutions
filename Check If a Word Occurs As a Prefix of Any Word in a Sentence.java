5f04079e096cb67611abfaf301b99ea888039ed0
jesseguitar87 <84287557+jesseguitar87@users.noreply.github.com>
2021-09-03 03:59:01 -0400

    Create Check If a Word Occurs As a Prefix of Any Word in a Sentence.java

---
+++ Check If a Word Occurs As a Prefix of Any Word in a Sentence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] splits = sentence.split(" ");
+        for (int i = 0; i < splits.length; i++) {
+            String str = "";
+            for (int j = 0; j < splits[i].length(); j++) {
+                str += splits[i].charAt(j);
+                if (str.equals(searchWord)) {
+                    return i + 1;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(splits));
+        return -1;
+    }
+}
